5f06cc7f1b4f971b61ed6dd28b77713ddb495d8b
Tessakam <tessa.kam@hotmail.com>
2020-10-15 10:36:15 +0200

    added required attributes with constructor and getters

--- Post.php
+++ Post.php
@@ -3,5 +3,67 @@
 
 class Post
 {
+//required attributes
+    private string $title, $content, $firstName, $lastName;
+    private DateTime $date;
+
+    /**
+     * Post constructor.
+     * @param string $title
+     * @param string $content
+     * @param string $firstName
+     * @param string $lastName
+     * @param DateTime $date
+     */
+    public function __construct(string $title, string $content, string $firstName, string $lastName, DateTime $date)
+    {
+        $this->title = $title;
+        $this->content = $content;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->date = $date;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDate(): DateTime
+    {
+        return $this->date;
+    }
+
 
 }
+
